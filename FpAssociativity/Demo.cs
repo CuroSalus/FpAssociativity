832a806edbed93cbfbd749a1c2909a6d770ebbac
CuroSalus <danieltholtzclaw@gmail.com>
2022-03-26 15:34:54 -0500

    Migrate code.

---
+++ FpAssociativity/Demo.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FpAssociativity
+{
+	internal static class Demo
+	{
+		const int NUM_TESTS = 5;
+
+		/// <summary>
+		/// Performs five tests, running <paramref name="size"/> number of
+		/// operations, with an <paramref name="offset"/> away from zero.
+		/// </summary>
+		/// <param name="size">Number of operations to run.</param>
+		/// <param name="offset">Offset from 0.</param>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// If either <paramref name="size"/> is less than 1
+		/// or <paramref name="offset"/> is not an acceptable value.
+		/// </exception>
+		public static void Run(int size, double offset)
+		{
+			if (size < 1)
+			{
+				throw new ArgumentOutOfRangeException(
+					nameof(size),
+					size,
+					"Size must be greater than 1."
+				);
+			}
+			if (
+				double.IsNaN(offset)
+				|| offset == double.PositiveInfinity
+				|| offset == double.NegativeInfinity
+				)
+			{
+				throw new ArgumentOutOfRangeException(
+					nameof(offset),
+					offset,
+					"Offset must be valid."
+				);
+			}
+
+			// Perform even number of operations.
+			if (size % 2 == 1) size++;
+			Random randomProvider = new();
+
+			Console.Clear();
+			Console.WriteLine(
+				$"Running tests (size: {size}, offset: {offset}){Environment.NewLine}"
+			);
+
+
+			double[] results = new double[NUM_TESTS];
+			double[] seed = new double[size];
+
+			// Generate values.
+			for (int i = 0; i < size; i += 2)
+			{
+				double randomValue = randomProvider.NextDouble();
+				seed[i] = randomValue + offset;
+				seed[i + 1] = (-randomValue + offset);
+			}
+
+			// Run accumulations.
+			for (int i = 0; i < NUM_TESTS; i++)
+			{
+				double[] values = FisherYates(randomProvider, seed);
+				double total = 0;
+				for (int k = 0; k < size; k += 2)
+				{
+					total += values[k];
+					total += values[k + 1];
+				}
+				results[i] = total;
+			}
+
+			// Display results.
+			Console.WriteLine($"Expected output: {offset}");
+			
+			for (int i = 0; i < NUM_TESTS; i++)
+			{
+				Console.WriteLine($"\t{i+1}: {results[i]}");
+			}
+		}
+
+		/// <summary>
+		/// Copies an array and then shuffles it, returning the shuffled copy.
+		/// </summary>
+		/// <typeparam name="T">Generic type of the array.</typeparam>
+		/// <param name="randomProvider">Provider of random values.</param>
+		/// <param name="array">Array to copy and shuffle.</param>
+		/// <returns>Shuffled copy of the array.</returns>
+		private static T[] FisherYates<T>(Random randomProvider, T[] array)
+		{
+			// Copy values.
+			T[] copy = new T[array.Length];
+			Array.Copy(array, 0, copy, 0, array.Length);
+
+			// Perform FisherYates.
+			int n = copy.Length;
+			while (n > 1)
+			{
+				int m = randomProvider.Next(n--);
+				(copy[m], copy[n]) = (copy[n], copy[m]);
+			}
+
+			return copy;
+		}
+	}
+}
---
+++ FpAssociativity/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace FpAssociativity
+{
+	/// <summary>
+	/// This application shows that floating-point operations are not guaranteed
+	/// to be associative.<para />
+	/// 
+	/// I.e.: A + (B + C) =/= (A + B) + C<para />
+	/// 
+	/// This is caused by minute inaccuracies in precision accumulating over many
+	/// operations, especially as the numbers grow more imprecise (further away
+	/// from zero). Inaccuracies may be observable even for small numbers of
+	/// operations close to 0. The closer a given digit is to the small-end of
+	/// the floating-point structure's precision, the more likely it is to be
+	/// affected.<para />
+	/// 
+	/// The demonstration is performed by creating an array of numbers with their
+	/// negated values around an offset (ex. 5.05 and -5.05, where the offset is
+	/// '5'), and then adding them together in a different order. The end result
+	/// should be the same as the offset (when solved analytically, this is
+	/// true), butt because the values are added in different orders, the result
+	/// has a chance to be difference.<para/>
+	/// 
+	/// Below, the "size" variable represents the number of operations and
+	/// "offset" represents the distance away from zero.
+	/// </summary>
+	internal class Program
+	{
+		const string INVALID_INPUT_MESSAGE = "Input was invalid. Try again.";
+
+		static void Main(string[] args)
+		{
+			int size = 0;
+			double offset = 0;
+			bool valid = false;
+
+			while (!valid)
+			{
+				Console.Write("Enter the number of FP operations to add: ");
+				string? input = Console.ReadLine();
+
+				if (int.TryParse(input, out size))
+				{
+					if (size > 1)
+					{
+						valid = true;
+					}
+				}
+				
+				if (!valid)
+				{
+					Console.Clear();
+					Console.WriteLine(INVALID_INPUT_MESSAGE);
+				}
+			}
+
+			valid = false;
+
+			while (!valid)
+			{
+				Console.Write("Enter the offset (default = 1): ");
+				string? input = Console.ReadLine();
+
+				if (string.IsNullOrEmpty(input))
+				{
+					offset = 1;
+					valid = true;
+				}
+				else
+				{
+					if (double.TryParse(input, out offset))
+					{
+						valid = true;
+					}
+					else
+					{
+						Console.Clear();
+						Console.WriteLine(INVALID_INPUT_MESSAGE);
+					}
+				}
+			}
+
+			Demo.Run(size, offset);
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key to close...");
+			Console.ReadKey(true);
+		}
+	}
+}
